a2adfeecb7d1b47a60b9bd47f1f9bfe01172449b
Tom <tom.jackson442@hotmail.com>
2021-12-05 17:31:57 +0000

    sensors

--- analyze.py
+++ analyze.py
@@ -1,7 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load("data/backLegSensorValues.npy")
+frontLegSensorValues = np.load("data/backLegSensorValues.npy")
 
-#pyplot.plot(backLegSensorValues)
-#pyplot.show()
+plt.plot(backLegSensorValues,linewidth=2)
+plt.legend()
+plt.show()
+plt.plot(frontLegSensorValues,linewidth=2)
+plt.legend()
+plt.show()
--- simulate.py
+++ simulate.py
@@ -14,15 +14,19 @@ p.setGravity(0,0,-9.8)
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
 backLegSensorValues = np.zeros(steps)
+frontLegSensorValues = np.zeros(steps)
 
 
 for i in range(0,steps):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+    frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
 
 p.disconnect()
 
 np.save("data/backLegSensorValues.npy",backLegSensorValues)
+np.save("data/frontLegSensorValues.npy",frontLegSensorValues)
+
 
 #print(backlogSensorValues)
