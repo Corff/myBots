c3de19f5518e9121aca6619bc61224e58d3e7808
Tom <tom.jackson442@hotmail.com>
2021-12-05 16:41:59 +0000

    eeee

---
+++ analyze.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as pyplot
+
+backLegSensorValues = np.load("data/backLegSensorValues.npy")
+
+#pyplot.plot(backLegSensorValues)
+#pyplot.show()
--- simulate.py
+++ simulate.py
@@ -3,10 +3,8 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
-import sys
 
-print(sys.executable)
-
+steps = 100
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
@@ -15,12 +13,16 @@ bodyID = p.loadURDF("body.urdf")
 p.setGravity(0,0,-9.8)
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
-backlogSensorValues = numpy.zeros(10000)
+backLegSensorValues = np.zeros(steps)
 
-for i in range(0,1000):
+
+for i in range(0,steps):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print(backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(1/60)
 
 p.disconnect()
+
+np.save("data/backLegSensorValues.npy",backLegSensorValues)
+
+#print(backlogSensorValues)
