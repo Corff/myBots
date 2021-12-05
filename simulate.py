a80ef23d73b3e5c57b65468fb1ed6d0759928342
Tom <tom.jackson442@hotmail.com>
2021-12-05 20:46:41 +0000

    es

--- simulate.py
+++ simulate.py
@@ -3,22 +3,40 @@ import pybullet_data
 import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
+import random
 
-steps = 100
+pi = 3.14159265359
+steps = 1000
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
 planeID = p.loadURDF("plane.urdf")
-bodyID = p.loadURDF("body.urdf")
+robot = p.loadURDF("body.urdf")
 p.setGravity(0,0,-9.8)
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
 backLegSensorValues = np.zeros(steps)
 frontLegSensorValues = np.zeros(steps)
-
+x = np.linspace(-np.pi, np.pi, 201)
 
 for i in range(0,steps):
     p.stepSimulation()
+
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robot,
+        jointName = "Torso BackLeg",
+        controlMode = p.POSITION_CONTROL,
+        targetPosition = random.uniform(-pi/2,pi/2),
+        maxForce = 25
+    )
+    pyrosim.Set_Motor_For_Joint(
+        bodyIndex = robot,
+        jointName = "Torso FrontLeg",
+        controlMode = p.POSITION_CONTROL,
+        targetPosition= random.uniform(-pi/2,pi/2),
+        maxForce = 25
+    )
+
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
     time.sleep(1/60)
