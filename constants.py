142aa23b60c2b064375b5306b0ed3830373cf21a
Tom <tom.jackson442@hotmail.com>
2021-12-06 01:15:35 +0000

    po

---
+++ constants.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+steps = 10000
+amplitude = np.pi/4
+frequency = 10,
+phaseOffset = 0
+maxForce = 500
+sleepTime = 1/60
--- simulate.py
+++ simulate.py
@@ -4,11 +4,7 @@ import time
 import pyrosim.pyrosim as pyrosim
 import numpy as np
 import random
-
-pi = 3.14159265359
-steps = 10000
-
-amplitude, frequency, phaseOffset = np.pi/4, 10, 0
+import constants as c
 
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
@@ -18,12 +14,12 @@ robot = p.loadURDF("body.urdf")
 p.setGravity(0,0,-9.8)
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate("body.urdf")
-backLegSensorValues = np.zeros(steps)
-frontLegSensorValues = np.zeros(steps)
-y = amplitude * np.sin(frequency * np.linspace(-np.pi, np.pi, 1000) + phaseOffset)
+backLegSensorValues = np.zeros(c.steps)
+frontLegSensorValues = np.zeros(c.steps)
+y = c.amplitude * np.sin(c.frequency * np.linspace(-np.pi, np.pi, 1000) + c.phaseOffset)
 np.save("data/sin",y)
 
-for i in range(0,steps):
+for i in range(0,c.steps):
     p.stepSimulation()
 
     pyrosim.Set_Motor_For_Joint(
@@ -31,14 +27,14 @@ for i in range(0,steps):
         jointName = "Torso BackLeg",
         controlMode = p.POSITION_CONTROL,
         targetPosition = y[i],
-        maxForce = 25
+        maxForce = c.maxForce
     )
     pyrosim.Set_Motor_For_Joint(
         bodyIndex = robot,
         jointName = "Torso FrontLeg",
         controlMode = p.POSITION_CONTROL,
         targetPosition= -y[i],
-        maxForce = 25
+        maxForce = c.maxForce
     )
 
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
